56060f6e39af347d3d52ca6a7fd4b9ce98bc07ff
Gome123 <78021316+Gome123@users.noreply.github.com>
2021-03-23 09:06:14 +0100

    Update bricks.js

--- js/bricks.js
+++ js/bricks.js
@@ -210,13 +210,13 @@ function drawIt() {
         clearInterval(refreshInterval);
         //game over animacija
         if (niz == 'gameOver') {
-            gameover.style.backgroundImage = "url('../img/gameOver.png')";
+            gameover.style.backgroundImage = 'url("../img/gameOver.png")';
             gameover.style.display = 'block';
             gameover.style.animation = 'fadein 1s';
             gameover.style.marginTop = ((-visinaCan / 2) - gameover.clientHeight / 2) + 'px';
             gameover.style.marginLeft = ((canvas.offsetLeft + sirinaCan / 2) - gameover.clientWidth / 2) + 'px';
         } else if (niz == 'youWin') {
-            gameover.style.backgroundImage = "url('../img/youWin.png')";
+            gameover.style.backgroundImage = 'url("../img/youWin.png")';
             gameover.style.display = 'block';
             gameover.style.animation = 'fadein 1s';
             gameover.style.marginTop = -visinaCan / 2 + 'px';
